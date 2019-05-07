b9ac2c95221b3b894c34f534ad2aebf6353a1f4d
Fardeen Khan <khanfardeen9983@gmail.com>
2019-05-07 13:09:35 +0530

    Multi dimension arrays

--- Collections/Arrays.cs
+++ Collections/Arrays.cs
@@ -23,7 +23,22 @@ namespace C_sharp_snippets.Collections
             //Dynamic initialization of array
             string[] names = new string[4];
             names[0] = "hey";
-            
+
+
+            //2-D arrays: An array of arrays
+            //Definition : Put a comma between braces
+            //Each subarray has to have the same number of elements
+            int[,] grid = {
+                { 0,1,2,3},
+                { 4,5,6,7},
+                { 8,9,10,11}
+            };
+            //Dynamic definiton
+            int[,] array = new int[2, 3];
+
+            //Accessing 2D array elements
+            Console.WriteLine(grid[0, 1]);
+
         }
     }
 }
