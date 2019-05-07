cebff2956098947284a04a8ad04db0630f20deb0
Fardeen Khan <khanfardeen9983@gmail.com>
2019-05-07 16:14:47 +0530

    Static fields and methods

--- Classes/Basics.cs
+++ Classes/Basics.cs
@@ -17,16 +17,27 @@ namespace C_sharp_snippets.Classes
         public string title, author;
         public int pages;
 
+        //Static varaibles are accessible through the class name alone and dont need an object to be created and accessed through dot operator
+        //Also they are shared by all the objects creates. Here the variable bookCount will keep track of books created by incrementing it whenever a Book object is created.
+        public static int bookCount;
+
+        //As private members of class cant be accessed directly by objects, so is the case for static memebers which can be accessed by static/non-static member functions
+        //Also it is customary to have the name of constant fields in uppercase
+        private static readonly int BOOK_MANAGER_ID = 899;
+
         //Constructor: It initialises the object of the Book class and is the method we call to get a new object
         //This is an empty constructor. It initialises the data members to some garbaage value
-        public Book() { }
+        public Book() { bookCount++; }
         //This is a parameterised constructor. It takes value for the data members
+
         public Book(string title, string author, int pages)
         {
             //this keyword refers to the instance of the Class being used
             this.author = author;
             this.title = title;
             this.pages = pages;
+
+            bookCount++;
         }
 
         //This is a copy constructor which copies it's values from existing object of the same type
@@ -35,12 +46,20 @@ namespace C_sharp_snippets.Classes
             this.author = b.author;
             this.title = b.title;
             this.pages = b.pages;
+
+            bookCount++;
         }
 
         //We can also generate a string equivalent of the class to describe it better
         public override string ToString()
         {
             return "Book details:\nAuthor : " + author + "\nTitle : " + title + "\nPages : " + pages;
+        }
+
+        //Define a static method to print the BOOK_MANAGER_ID
+        public static int getBookManagerId()
+        {
+            return BOOK_MANAGER_ID;
         }
     }
 
@@ -57,6 +76,10 @@ namespace C_sharp_snippets.Classes
             book1.pages = 200;
             Console.WriteLine(book1.pages);
             Console.WriteLine(book3);
+            
+            Console.WriteLine("\nBooks count : " + Book.bookCount);
+
+            Console.WriteLine("\nBook Manager ID : " + Book.getBookManagerId());
         }
     }
 }
