7dc5758f08258fca51c3758e0d8c8c95d25f6cb8
Fardeen Khan <khanfardeen9983@gmail.com>
2019-05-07 12:12:36 +0530

    IF else and switch conditionals

---
+++ Getting-started/Ifelse.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_sharp_snippets.Getting_started
+{
+    class Conditionals
+    {
+        static void Main(string[] args)
+        {
+            //Conditonal statements like if else allow the program to follow different program executions based on some conditions
+            // These conditions are relational expressions
+
+            //Find the biggest nummber of 3
+            int a, b, c;
+            Console.WriteLine("Enter 3 numbers: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            c = Convert.ToInt32(Console.ReadLine());
+
+            int max = -1;
+            if (a > b)
+            {
+                if (a > c) max = a;
+                else max = c;
+            }
+            else
+            {
+                if (b > c) max = b;
+                else max = c;
+            }
+
+            Console.WriteLine("Maximum of a,b,c : " + max);
+
+            //Logical operators && (and) , || (or) , ! (not)
+            if(5>4 && 5 > 6)
+            {
+                //Executed only when both LHS and RHS are true
+                Console.WriteLine("Can never be executed");
+            }
+
+            if(5>4 || 5 > 6)
+            {
+                //Executed only when either of the LHS and RHS are true
+                Console.WriteLine("5>4");
+            }
+
+            if(!(5 > 6))
+            {
+                //Executed only when the condition after ! is false
+                Console.WriteLine("5 is not greater than 6");
+            }
+        }
+    }
+}
---
+++ Getting-started/Switch.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_sharp_snippets.Getting_started
+{
+    class Switch
+    {
+        static void Main(string[] args)
+        {
+            //Switch statements take a constant as input and it checks it's value for a match against a set of cases of the same types
+            //If a match is found then the code associated with that case is executed or the default case is executed if it is defined
+
+            //Simple calculator with Switch
+            Console.WriteLine("Enter two numbers");
+            double a = Convert.ToDouble(Console.ReadLine()), b = Convert.ToDouble(Console.ReadLine()), res;
+            Console.WriteLine("Enter operator : ( + , -, * , / , % )");
+            char op = Console.ReadKey().KeyChar;
+            res = -1;
+            switch (op)
+            {
+                case '+': res = a + b; break;
+                case '-': res = a - b; break;
+                case '/': res = a / b; break;
+                case '%': res = a % b; break;
+                case '*': res = a * b; break;
+                default : Console.WriteLine("Please enter valid operator"); break; 
+            }
+
+            Console.WriteLine("\nResult : " + res);
+        }
+    }
+}
